a444e07a4d8dbdc064b4905182cc2b48e9cf5015
Anthony <ahall012@ucr.edu>
2019-04-27 00:08:21 -0700

    Started the basics of the scripts

---
+++ scripts/mainMenu.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+loop=true
+
+if [[ $EUID -ne 0 ]]; then
+    echo "\nScript must be run as root\n"
+    exit 1
+fi
+
+while [ loop ] do
+    echo "\nPlease select an option:"
+    echo "1 ) Show Networks"
+    echo "2 ) Attack Networks"
+    echo "3 ) quit"
+    read -r option
+
+    if [[ -z "$option" ]]; then
+        echo "Not a valid option"
+    fi
+
+    if [[ "$option" == 1 ]]; then 
+        
+    fi
+
+    if [[ "$option" == 2 ]]; then
+
+    fi
+
+    if [[ "$option" == 3 ]]; then
+        loop = false
+    fi
+
+done
