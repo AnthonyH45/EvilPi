8499208820494af220b3640dd5faaef860255336
Luis Garcia <luisgarcia@ucrwpa-6-2-10-28-39-161.wnet.ucr.edu>
2019-04-27 14:33:19 -0700

    stable menu rn

--- scripts/menu.py
+++ scripts/menu.py
@@ -1,34 +1,31 @@
 import sys
 import subprocess
 
-def asciiArt():
-    
-
 def menu():
-    print("1) Show Networks\n2) Attack Networks\n3) Quit\n")
-    userInput = input(">")
+    print("1) Show Networks\n2) Attack Networks\n3) Quit")
+    userInput = input("> ")
 
     if(userInput=="1"):
-        subprocess.Popen("./showNetworks.sh", ">", "interfaces.txt")
+        subprocess.Popen("./showNetworks.sh > interfaces.txt", shell=True)
         print("Available Networks: ")
-        subprocess.Popen(["cat","interfaces.txt"])
+        subprocess.Popen("cat interfaces.txt", shell=True)
 
     elif(userInput=="2"):
-        print("Enter Attack Option\n1) Only Capture Handshakes\n2) Only Aggressive Capture Handshakes\n3) Capture and Send to GCP to crack\n4) Agressive Capture and Send to GCP to Crack\n5) Evil Twin (Bwahaha)\n6) Deauth Option\n")
-        userInput == input(">")
+        print("Enter Attack Option\n1) Only Capture Handshakes\n2) Only Aggressive Capture Handshakes\n3) Capture and Send to GCP to crack\n4) Agressive Capture and Send to GCP to Crack\n5) Evil Twin (Bwahaha)\n6) Deauth Option")
+        userInput == input("> ")
 
         if(userInput=="1"):
-            sys.call("./captureHandshakes.sh")
+            subprocess.Popen("./captureHandshakes.sh", shell=True)
         elif(userInput=="2"):
-            sys.call("./aggressiveCapture.sh")
+            subprocess.Popen("./aggressiveCapture.sh", shell=True)
         elif(userInput=="3"):
-            sys.call("./captureHandshakes.sh; ./sendToGCP.sh")
+            subprocess.Popen("./captureHandshakes.sh; ./sendToGCP.sh", shell=True)
         elif(userInput=="4"):
-            sys.call("./aggressiveCapture.sh; ./sendToGCP.sh")
+            subprocess.Popen("./aggressiveCapture.sh; ./sendToGCP.sh", shell=True)
         elif(userInput=="5"):
-            sys.call("./evilTwin.sh")
+            subprocess.Popen("./evilTwin.sh", shell=True)
         elif(userInput=="6"):
-            sys.call("./deauth.sh")
+            subprocess.Popen("./deauth.sh", shell=True)
     elif(userInput=="3"):
         print("Bye")
         sys.exit(0)
