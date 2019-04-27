7dbc4fc55bcd8b0bb44a2ccdc1a516ce1d5e8afc
Anthony <ahall012@ucr.edu>
2019-04-27 01:18:12 -0700

    Fixed the quit in main menu

--- scripts/mainMenu.sh
+++ scripts/mainMenu.sh
@@ -1,34 +1,43 @@
 #!/bin/bash
 
-loop=true
-
 if [[ $EUID -ne 0 ]]; then
-    echo "\nScript must be run as root\n"
+    echo "Script must be run as root"
     exit 1
 fi
 
-while [ loop ] do
-    echo "\nPlease select an option:"
+loop=true
+
+while [ $loop = true ]
+do
+    echo ""
+    echo "Please select an option:"
     echo "1 ) Show Networks"
     echo "2 ) Attack Networks"
     echo "3 ) quit"
+    echo ""
     read -r option
 
     if [[ -z "$option" ]]; then
+        echo ""
         echo "Not a valid option"
+        echo ""
     fi
 
     if [[ "$option" == 1 ]]; then 
         ./showNetworks.sh > interfaces.txt
-        echo "available networks: " for i in 'cat interfaces.txt'; do echo $2; done
+        #echo "available networks: " for i in 'cat interfaces.txt'; do echo $2; done
     fi
 
     if [[ "$option" == 2 ]]; then
-        
+        echo ""
+        echo "Enter BSSID: "
+        read -r victimBSSID
+        echo "Enter channel: "
+        read -r victimChannel
+        echo ""
     fi
 
     if [[ "$option" == 3 ]]; then
-        loop = false
+        loop=false
     fi
-
 done
