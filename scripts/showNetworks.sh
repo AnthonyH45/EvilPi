92361c6bba1208bd03bc296a40fd123d8635954a
Anthony <ahall012@ucr.edu>
2019-04-27 16:28:05 -0700

    showNetwork.sh

--- scripts/showNetworks.sh
+++ scripts/showNetworks.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 interface=$(cat interfaceToUse.txt)
-airodump-ng $interface -w networks --write-interval 30 -o csv
+airodump-ng $interface -w networks --write-interval 30 -o csv & > /dev/null
+PID=$!
+wait(1000)
+kill $PID
