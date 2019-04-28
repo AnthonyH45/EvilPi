32fc47e329a927b67324208fe3b7adc0c4cd4e76
PatrickWumbo <jhuh009@ucr.edu>
2019-04-28 01:56:30 +0000

    Update on menu interface

--- scripts/menu.py
+++ scripts/menu.py
@@ -22,23 +22,41 @@ def menu():
     if(userInput=="1"):
         subprocess.Popen("./showNetworks.sh", shell=True)
         print("Available Networks: ")
-        subprocess.Popen("cat networks.txt", shell=True)
+        subprocess.call("cat networks.txt", shell=True)
+        print("")
 
     elif(userInput=="2"):
-        print("Enter Attack Option\n1) Only Capture Handshakes\n2) Only Aggressive Capture Handshakes\n3) Capture and Send to GCP to crack\n4) Agressive Capture and Send to GCP to Crack\n5) Evil Twin (Bwahaha)\n6) Deauth Option")
-        userInput = input("> ")
-        if(userInput=="1"):
-            subprocess.Popen("./captureHandshakes.sh", shell=True)
-        elif(userInput=="2"):
-            subprocess.Popen("./aggressiveCapture.sh", shell=True)
-        elif(userInput=="3"):
-            subprocess.Popen("./captureHandshakes.sh; ./sendToGCP.sh", shell=True)
-        elif(userInput=="4"):
-            subprocess.Popen("./aggressiveCapture.sh; ./sendToGCP.sh", shell=True)
-        elif(userInput=="5"):
-            subprocess.Popen("./evilTwin.sh", shell=True)
-        elif(userInput=="6"):
-            subprocess.Popen("./deauth.sh", shell=True)
+        while(True):
+            print("Enter Attack Option\n1) Only Capture Handshakes\n2) Only Aggressive Capture Handshakes\n3) Capture and Send to GCP to crack\n4) Agressive Capture and Send to GCP to Crack\n5) Evil Twin (Bwahaha)\n6) Deauth Option\n7) Main Menu")
+            userInput = input("> ")
+            if(userInput=="1"):
+                PROCESS=subprocess.Popen("./captureHandshakes.sh", shell=True)
+                PROCESS.wait()
+                break
+            elif(userInput=="2"):
+                PROCESS=subprocess.Popen("./aggressiveCapture.sh", shell=True)
+                PROCESS.wait()
+                break
+            elif(userInput=="3"):
+                PROCESS=subprocess.Popen("./captureHandshakes.sh; ./sendToGCP.sh", shell=True)
+                PROCESS.wait()
+                break
+            elif(userInput=="4"):
+                PROCESS=subprocess.Popen("./aggressiveCapture.sh; ./sendToGCP.sh", shell=True)
+                PROCESS.wait()
+                break
+            elif(userInput=="5"):
+                PROCESS=subprocess.Popen("./evilTwin.sh", shell=True)
+                PROCESS.wait()
+                break
+            elif(userInput=="6"):
+                PROCESS=subprocess.Popen("./deauth.sh", shell=True)
+                PROCESS.wait()
+                break
+            elif(userInput=="7"):
+                break
+            else:
+                print("Wrong option. Please try again.")
     elif(userInput=="3"):
         print("Bye")
         stopInterface()
