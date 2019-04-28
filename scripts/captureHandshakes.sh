c310a8e5cdb0893b213f16305b88afb47540d894
PatrickWumbo <jhuh009@ucr.edu>
2019-04-28 16:49:39 +0000

    fixed mismatch issue

--- scripts/captureHandshakes.sh
+++ scripts/captureHandshakes.sh
@@ -8,10 +8,14 @@ cat network-list.txt
 read -r input
 
 ssid=$(awk -v pat="$input" -F ":" '$0~pat{print $2}' network-list.txt)
-
-awk -v pat="$ssid" -F" " '$0~pat{print $2" "$3}' networks.txt > combined.txt
+#echo $ssid
+awk -v pat="$ssid" -F" " '$0~patFNR<7{print $2" "$3}' networks.txt > combined.txt
 interface=$(cat interfaceToUse.txt)
 targetChannel=$(awk -F " " 'FNR<2{print $2}' combined.txt)
 targetBSSID=$(awk -F " " 'FNR<2{print $1}' combined.txt)
+
+#echo $interface
+#echo $targetChannel
+#echo $targetBSSID
 
 airodump-ng -c $targetChannel --bssid $targetBSSID -w psk $interface
