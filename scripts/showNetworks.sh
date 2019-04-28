4c1c3618cc3a78f1e44f4caeb75f57ff18d507bc
PatrickWumbo <jhuh009@ucr.edu>
2019-04-28 03:52:27 +0000

    Displays unique network names

--- scripts/showNetworks.sh
+++ scripts/showNetworks.sh
@@ -2,6 +2,6 @@
 nmcli -f in-use,ssid,bssid,chan dev wifi > networks.txt
 num=0
 awk -F " " '!seen[$1]++' networks.txt > test.txt
-awk -F " " '{print $1}' test.txt | grep -n > help.txt
+awk -F " " 'FNR > 1{print $1}' test.txt | grep ".*" -n > help.txt
 
 
